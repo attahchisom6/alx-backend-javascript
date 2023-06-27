60928732a3838536f15a0f29afa4b794c12635b0
attahchisom6 <attahchisom6@gmail.com>
2023-06-27 13:08:54 +0000

    adding

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,8 +3,8 @@ export default function createReportObject(employeesList) {
     allEmployees: {
       ...employeesList,
     },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
     },
   };
 }
