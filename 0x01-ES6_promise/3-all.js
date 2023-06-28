3e0368db4604ada9ecc2d7d7aafbc28d58139290
attahchisom6 <attahchisom6@gmail.com>
2023-06-28 02:17:21 +0000

    uploaing photo anf other info from other files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const utilsInfo = [uploadPhoto(), createUser()];
-  const promise = Promise.all(utilsInfo);
-  return promise
+  return Promise.all(utilsInfo)
     .then((utilsInfo) => console.log(`${utilsInfo[0].body}, ${utilsInfo[1].firstName}, ${utilsInfo[1].lastName}`))
     .catch(() => console.log('Signup system offline '));
 }
