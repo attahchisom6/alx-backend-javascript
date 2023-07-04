0e6b9106494c6db931341a433dba1ce60c900cbe
attahchisom6 <attahchisom6@gmail.com>
2023-07-04 17:45:03 +0000

    weak point'

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-let count = 1;
 export default function queryAPI(endpoint) {
-  // let count = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, count);
-  count += 1;
-  if (weakMap.get(endpoint) >= 5) {
+  let count = weakMap.get(endpoint) || 0;
+
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
+
+  weakMap.set(endpoint, count + 1);
 }
