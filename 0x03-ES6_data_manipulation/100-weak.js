fd5d3daffd4e77c58234cae81d38f03991431989
attahchisom6 <attahchisom6@gmail.com>
2023-07-04 17:59:47 +0000

    wrak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
-
-  if (count >= 5) {
+let count = 1;
+const queryAPI = (endpoint) => {
+  weakMap.set(endpoint, count);
+  count += 1;
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
+};
 
-  weakMap.set(endpoint, count + 1);
-}
+export { weakMap, queryAPI };
