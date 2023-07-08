57fe75f507328af1446fd09fd3ff9f69d8e37b8d
attahchisom6 <attahchisom@gmail.com>
2023-07-08 17:03:12 +0100

    adding

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,44 @@
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [keyName: string]: any,
+};
+
+class TeacherClass implements Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number | undefined;
+    location: string;
+    [keyName: string]: any;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullTimeEmployee = false;
+        this.location = '';
+    }
+}
+
+const Teacher3: Teacher = new TeacherClass('john', 'Doe');
+Teacher3.fullTimeEmployee = false;
+Teacher3.location = 'London';
+console.log(Teacher3);
+
+interface Directors extends Teacher {
+
+    numberOfReports: number,
+}
+
+const Director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: true,
+    location: 'London',
+    numberOfReports: 17,
+}
+
+console.log(Director1);
