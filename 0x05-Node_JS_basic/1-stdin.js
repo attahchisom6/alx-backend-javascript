461b2a2fcb4a7f43fbe11e5ce4098ee6d3a907d5
attahchisom6 <attahchisom@gmail.com>
2023-08-21 13:47:29 +0100

    STDING: file sysyem input and output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,22 +2,18 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (name) => {
-  if (name.toLowerCase() == "exit" || name.toLowerCase() == "quit") {
-    console.log("This important software is now closing");
-    rl.close();
-  }
   console.log(`Your name is: ${name}`);
-  console.log("Welcome to Holberton School, what is your name?");
-
+  console.log('This important software is now closing');
+  rl.close();
 });
 
 // listen for closing event
 rl.on('close', () => {
   process.exit(0);
-})
+});
