4d0bf5f0fe615c79469696161bc99c72ddbd4ac0
attahchisom6 <attahchisom@gmail.com>
2023-08-21 17:48:18 +0100

    adding

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,26 +4,26 @@ function countStudents(path) {
   const promise = new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       resolve(data);
     });
   });
 
-  return promise.then(data => {
-    const lines = data.split("\n");
-    const headerList = ["firstname", "lastname", "age", "field"];
-    const headers = lines[0].split(",");
+  return promise.then((data) => {
+    const lines = data.split('\n');
+    const headerList = ['firstname', 'lastname', 'age', 'field'];
+    const headers = lines[0].split(',');
 
     for (let k = 0; k < headerList.length; k += 1) {
       if (headers[k] !== headerList[k]) {
-        throw new Error("Invalid format");
+        throw new Error('Invalid format');
       }
     }
 
-    studentByField = {};
+    const studentByField = {};
     for (let k = 1; k < lines.length; k += 1) {
-      const fields = lines[k].split(",");
+      const fields = lines[k].split(',');
       if (fields.length === 4) {
         const field = fields[3];
 
@@ -39,11 +39,11 @@ function countStudents(path) {
 
     for (const field in studentByField) {
       if (field) {
-        const list = studentByField[field].join(",");
+        const list = studentByField[field].join(',');
         console.log(`Number of students in ${field}: ${studentByField[field].length}. List: ${list}`);
       }
     }
   });
-};
+}
 
 module.exports = countStudents;
