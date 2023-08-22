5239952dd97d84198a59be37319626a51eab59ed
attahchisom6 <attahchisom6@gmail.com>
2023-08-22 12:32:07 +0000

    add

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,19 +1,25 @@
 const http = require('http');
-const countStuents = requite('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   const header = { 'Content-type': 'text/plain' };
-  if (req.request === '/') {
+  if (req.url === '/') {
     res.writeHead(200, header);
     res.end('Hello Holberton School!');
-  } else if req.request === '/students') {
-      const result = countStudents('database.csv').then(() => {
-        console.log('After');
-      })
-  .catch(error => {
-    throw new Error('error');
-  });
-  res.end(`This is the list of our students\n${result}`);
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+    .then(result => {
+      res.writeHead(200, header);
+      res.end(`This is the list of our students\n${result}`)
+    })
+    .catch(error => {
+      res.writeHead(500, header);
+      res.end('Internal Server Error');
+    });
+  } else {
+    res.writeHead(404, header);
+    res.end("Page not found!");
+  }
 });
 
 app.listen(1245, '0.0.0.0');
