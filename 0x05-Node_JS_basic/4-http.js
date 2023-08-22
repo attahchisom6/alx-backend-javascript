4470e421f2d82b1c3611c9077b095023cb11d354
attahchisom6 <attahchisom6@gmail.com>
2023-08-22 14:02:08 +0000

    added

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,12 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  const header = { 'Content-Type': 'text/plain' };
-  res.writeHead(200, header);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, '127.0.0.1', () => {
-  console.log('Listening on port 1245')
+app.listen(1245, '0.0.0.0', () => {
+  console.log('Listening on port 1245');
 });
 
 module.exports = app;
-
