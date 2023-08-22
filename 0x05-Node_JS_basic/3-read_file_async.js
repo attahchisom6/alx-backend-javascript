45e054386266bb06dc577964b37991147f7b55d1
attahchisom6 <attahchisom6@gmail.com>
2023-08-22 12:53:57 +0000

    add

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents(path) {
+async function countStudents(path) {
   const promise = new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
