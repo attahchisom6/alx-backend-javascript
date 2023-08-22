f9d761d55d3d0d2c4075b76bbfd42f1030f60fca
attahchisom6 <attahchisom6@gmail.com>
2023-08-22 13:59:59 +0000

    add

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,27 +1,34 @@
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  const header = { 'Content-type': 'text/plain' };
-  if (req.url === '/') {
-    res.writeHead(200, header);
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents('database.csv')
-    .then(result => {
-      res.writeHead(200, header);
-      res.end(`This is the list of our students\n${result}`)
-    })
-    .catch(error => {
-      res.writeHead(500, header);
-      res.end('Internal Server Error');
-    });
-  } else {
-    res.writeHead(404, header);
-    res.end("Page not found!");
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(1245, '0.0.0.0');
+app.listen(port, hostname, () => { });
 
 module.exports = app;
