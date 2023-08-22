674de2dfb25d350fdc49fd7a66a644151ec8905c
attahchisom6 <attahchisom6@gmail.com>
2023-08-22 10:34:20 +0000

    all server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,9 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen('1245');
+app.listen('1245', '127.0.0.1', () => {
+  console.log('Listening on port 1245')
+});
 
 module.exports = app;
 
