49bb953fba1365cc8606c232babced5d58a13c9a
attahchisom6 <attahchisom6@gmail.com>
2023-08-24 17:43:50 +0000

    add

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,66 +1,66 @@
 const calculateNumber = require('./2-calcul_chai')
-const { expect } = require('expect');
+const { expect } = require('chai');
 
 describe('SUM tests', () => {
   it('Test for add negatives and positives', () => {
-    expect.strictEqual(calculateNumber('SUM', 4, 8), 12);
-    expect.strictEqual(calculateNumber('SUM', 1.9, 0), 2);
-    expect.strictEqual(calculateNumber('SUM', 6.1, 6.1), 12);
-    expect.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
-    expect.strictEqual(calculateNumber('SUM', 0.1, 0.6), 1);
-    expect.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    expect.strictEqual(calculateNumber('SUM', -1, -3), -4);
-    expect.strictEqual(calculateNumber('SUM', -1.4, -3.6), -5);
+    expect(calculateNumber('SUM', 4, 8)).to.equal(12);
+    expect(calculateNumber('SUM', 1.9, 0)).to.equal(2);
+    expect(calculateNumber('SUM', 6.1, 6.1)).to.equal(12);
+    expect(calculateNumber('SUM', 0.1, 0.2)).to.equal(0);
+    expect(calculateNumber('SUM', 0.1, 0.6)).to.equal(1);
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    expect(calculateNumber('SUM', -1, -3)).to.equal(-4);
+    expect(calculateNumber('SUM', -1.4, -3.6)).to.equal(-5);
   });
 
   it('TypeErrors', () => {
-    expect.throws(() => calculateNumber('SUM', NaN, 5.6), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('SUM', 5.6, NaN), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('SUM', NaN, NaN), { name: 'TypeError' });
+    expect(() => calculateNumber('SUM', NaN, 5.6)).to.throw(TypeError);
+    expect(() => calculateNumber('SUM', 5.6, NaN)).to.throw(TypeError);
+    expect(() => calculateNumber('SUM', NaN, NaN)).to.throw(TypeError);
   });
 });
 
 describe('SUBTRACT tests', () => {
   it('Substracts positive and negatives', () => {
-    expect.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
-    expect.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
-    expect.strictEqual(calculateNumber('SUBTRACT', 8, 4), 4);
-    expect.strictEqual(calculateNumber('SUBTRACT', 1.9, 0), 2);
-    expect.strictEqual(calculateNumber('SUBTRACT', 6.1, 6.1), 0);
-    expect.strictEqual(calculateNumber('SUBTRACT', 1, 0.2), 1);
-    expect.strictEqual(calculateNumber('SUBTRACT', 0.6, 0.1), 1);
-    expect.strictEqual(calculateNumber('SUBTRACT', 4.5, 1.4), 4);
+    expect(calculateNumber('SUBTRACT', -1, -3), 2);
+    expect(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
+    expect(calculateNumber('SUBTRACT', 8, 4)).to.equal(4);
+    expect(calculateNumber('SUBTRACT', 1.9, 0)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', 6.1, 6.1)).to.equal(0);
+    expect(calculateNumber('SUBTRACT', 1, 0.2)).to.equal(1);
+    expect(calculateNumber('SUBTRACT', 0.6, 0.1)).to.equal(1);
+    expect(calculateNumber('SUBTRACT', 4.5, 1.4)).to.equal(4);
   });
 
   it('TypeErrors', () => {
-    expect.throws(() => calculateNumber('SUBTRACT', NaN, 5.6), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('SUBTRACT', 5.6, NaN), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('SUBTRACT', NaN, NaN), { name: 'TypeError' });
+    expect(() => calculateNumber('SUBTRACT', NaN, 5.6)).to.throw(TypeError);
+    expect(() => calculateNumber('SUBTRACT', 5.6, NaN)).to.throw(TypeError);
+    expect(() => calculateNumber('SUBTRACT', NaN, NaN)).to.throw(TypeError);
   });
   
 });
 
 describe('DIVIDE', () => {
   it('positive and negative divide', () => {
-    expect.strictEqual(calculateNumber('DIVIDE', 8, 4), 2);
-    expect.strictEqual(calculateNumber('DIVIDE', 6.1, 1.7), 3);
-    expect.strictEqual(calculateNumber('DIVIDE', 6.1, 6.1), 1);
-    expect.strictEqual(calculateNumber('DIVIDE', -2, 1.1), -2);
-    expect.strictEqual(calculateNumber('DIVIDE', -4.4, -2.2), 2);
-    expect.strictEqual(calculateNumber('DIVIDE', 2.0, 1.1), 2);
-    expect.strictEqual(calculateNumber('DIVIDE', 0.6, 0.9), 1);
-    expect.strictEqual(calculateNumber('DIVIDE', 4.5, 5), 1);
+    expect(calculateNumber('DIVIDE', -2, 1.1), -2);
+    expect(calculateNumber('DIVIDE', -4.4, -2.2), 2);
+    expect(calculateNumber('DIVIDE', 8, 4), 2);
+    expect(calculateNumber('DIVIDE', 6.1, 1.7), 3);
+    expect(calculateNumber('DIVIDE', 6.1, 6.1), 1);
+    expect(calculateNumber('DIVIDE', 2.0, 1.1), 2);
+    expect(calculateNumber('DIVIDE', 0.6, 0.9), 1);
+    expect(calculateNumber('DIVIDE', 4.5, 5), 1);
   });
 
   it('returns', () => {
-    expect.strictEqual(calculateNumber('DIVIDE', 2, 0), 'Error');
-    expect.strictEqual(calculateNumber('DIVIDE', 3, 0), 'Error');
-    expect.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
+    expect(calculateNumber('DIVIDE', 2, 0), 'Error');
+    expect(calculateNumber('DIVIDE', 3, 0), 'Error');
+    expect(calculateNumber('DIVIDE', 4, 0), 'Error');
   });
 
   it('TypeErrors', () => {
-    expect.throws(() => calculateNumber('DIVIDE', NaN, 5.6), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('DIVIDE', 5.6, NaN), { name: 'TypeError' });
-    expect.throws(() => calculateNumber('DIVIDE', NaN, NaN), { name: 'TypeError' });
+    expect(() => calculateNumber('DIVIDE', NaN, 5.6)).to.throw(TypeError);
+    expect(() => calculateNumber('DIVIDE', 5.6, NaN)).to.throw(TypeError);
+    expect(() => calculateNumber('DIVIDE', NaN, NaN)).to.throw(TypeError);
   });
 });
