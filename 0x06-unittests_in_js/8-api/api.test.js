45f454752fee5fcd1828b1d7fa0b90d34aa304f5
attahchisom6 <attahchisom6@gmail.com>
2023-08-27 07:19:54 +0000

    added

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -8,24 +8,24 @@ describe('testing api return load', () => {
     method: 'GET',
   };
 
-  it('first test: correct status code', () => new Promise((done) => {
+  it('first test: correct status code', (done) => {
     request(urlParams, (err, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
-  }));
+  });
 
-  it('second Test: correct payload/result', () => new Promise((done) => {
+  it('second Test: correct payload/result', (done) => {
     request(urlParams, (err, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
-  }));
+  });
 
-  it('other test: invalid status code', () => new Promise((done) => {
+  it('other test: invalid status code', (done) => {
     request(urlParams, (err, response, body) => {
       expect(response.statusCode).to.not.equal(400);
       done();
     });
-  }));
+  });
 });
